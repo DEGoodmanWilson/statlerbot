8e85312711322a1333e97575aafff8bc5af2b804
Don Goodman-Wilson <don@slack-corp.com>
2016-09-15 07:44:51 -0700

    Add more witty banter

--- event_receiver.cpp
+++ event_receiver.cpp
@@ -54,6 +54,19 @@ event_receiver::handle_message(std::shared_ptr<slack::event::message> event, con
     static std::vector<std::string> phrases = {
             "I wonder if there really is life on another planet.",
             "Boo!",
+            "Hm. Do you think this channel is educational?",
+            "He was doing okay until he left the channel.",
+            "I liked that last message.",
+            "I'm going to see my lawyer!",
+            "You know, the older I get, the more I appreciate good wit.",
+            "That really offended me. I'm a student of Shakespeare.",
+            "I love it! I love it!",
+            "More! More!",
+            "I don't believe it! They've managed the impossible! What an achievement! Bravo, bravo!",
+            "I wonder if anybody reads this channel besides us?",
+            "You know, I think they were trying to make a point with that comment.",
+            "You know, that was almost funny.",
+            "Are you ready for the end of the world?",
     };
 
 
@@ -173,5 +186,129 @@ event_receiver::event_receiver(server *server, token_storage *store, const std::
     handler_.hears("You know, that's pretty catchy.", [](const auto &message)
     {
         message.reply("So is smallpox.");
+    });
+
+    handler_.hears("More! More!", [](const auto &message)
+    {
+        message.reply("No, not so loud: they may hear you!");
+    });
+
+    handler_.hears("Yeah, whadya think?", [](const auto &message)
+    {
+        message.reply("Beats sitting home watching television.");
+    });
+
+    handler_.hears("What did you like about it?", [](const auto &message)
+    {
+        message.reply("It was the _last_ message!");
+    });
+
+    handler_.hears("Have we ever said that this channel is for the birds?", [](const auto &message)
+    {
+        message.reply("Yes, and we'll keep saying it until it gets a laugh.");
+    });
+
+    handler_.hears("Do you think there's life in outer space?", [](const auto &message)
+    {
+        message.reply("There's certainly none in this channel.");
+    });
+
+    handler_.hears("Well, this has been a day to remember.", [](const auto &message)
+    {
+        message.reply("Why is that?");
+    });
+
+    handler_.hears("Why?", [](const auto &message)
+    {
+        message.reply("I'm going to find out if you can sue a team for breach of taste!");
+    });
+
+    handler_.hears(":one:", [](const auto &message)
+    {
+        message.reply("You gave him a one?");
+    });
+
+    handler_.hears("More! More!", [](const auto &message)
+    {
+        message.reply("Less! Less!");
+    });
+
+    handler_.hears("Yeah? What's that got to do with what we just read?", [](const auto &message)
+    {
+        message.reply("Nothing, just thought I'd mention it.");
+    });
+
+    handler_.hears("You know, I'm really going to enjoy today!", [](const auto &message)
+    {
+        message.reply("You plan to like this channel?");
+    });
+
+    handler_.hears(":tv: What's the name of this movie?", [](const auto &message)
+    {
+        message.reply("\"Beach Blanket Frankenstein\".");
+    });
+    handler_.hears("Awful.", [](const auto &message)
+    {
+        message.reply("Terrible film!");
+    });
+    handler_.hears("Yeah, well, we could read this channel instead.", [](const auto &message)
+    {
+        message.reply(":eyes:");
+    });
+    handler_.hears(":eyes:", [](const auto &message)
+    {
+        message.reply("Wonderful.");
+    });
+
+    handler_.hears("How do they do it?", [](const auto &message)
+    {
+        message.reply("How do _we read_ it?");
+    });
+    handler_.hears("_Why_ do we read it?", [](const auto &message)
+    {
+        message.reply("Why do _the rest of you_ read it?");
+    });
+
+    handler_.hears("What, you mean you actually like this channel now?", [](const auto &message)
+    {
+        message.reply("No, they've made the channel even worse!");
+    });
+
+    handler_.hears("Eh, this channel is good for what ails me.", [](const auto &message)
+    {
+        message.reply("Well, what ails ya?");
+    });
+
+    handler_.hears("That seemed like something very different.", [](const auto &message)
+    {
+        message.reply("Did you like it?");
+    });
+    handler_.hears("No.", [](const auto &message)
+    {
+        message.reply("Then it wasn't different.");
+    });
+
+    handler_.hears(":zzz:", [](const auto &message)
+    {
+        message.reply("Besides me?");
+    });
+
+    handler_.hears("Ohh...", [](const auto &message)
+    {
+        message.reply("What's wrong with you?");
+    });
+    handler_.hears("It's either this channel or indigestion. I hope it's indigestion.", [](const auto &message)
+    {
+        message.reply("Why indigestion?");
+    });
+
+    handler_.hears("What's the point?", [](const auto &message)
+    {
+        message.reply("You're right. Forget it.");
+    });
+
+    handler_.hears("That was a funny comment.", [](const auto &message)
+    {
+        message.reply("Yes, it was. I wonder if they meant it that way.");
     });
 }
